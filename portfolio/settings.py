13fef07791e871463cca49320ee357e91a600a1a
gunn03 <92280635+gunn03@users.noreply.github.com>
2023-01-16 14:36:50 +1300

    cloudinary deleted

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -39,16 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'james',
 
-    'cloudinary_storage',
-    'django.contrib.staticfiles',
-    'cloudinary',
 ]
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'your_cloud_name',
-    'API_KEY': 'your_api_key',
-    'API_SECRET': 'your_api_secret'
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
